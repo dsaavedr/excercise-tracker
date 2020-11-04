d388960225f117a5a5fabfede912ca8beeac3a5d
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-11-04 18:27:53 -0500

    Imported components and added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div id="App">
-      <h1>Hello from App!</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Route path="/" component={ExercisesList} exact></Route>
+        <Route path="/edit/:id" component={EditExercise}></Route>
+        <Route path="/create" component={CreateExercise}></Route>
+        <Route path="/user" component={CreateUser}></Route>
+      </div>
+    </Router>
   );
 }
-
-export default App;
