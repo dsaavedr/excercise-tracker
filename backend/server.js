a80a29a2423d0cba6100f6056faf0c7c30e56243
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-11-04 14:48:48 -0500

    Created backend NPM project, added packages and connected to Mongo db

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+// Body parser apparently not needed in newer versions of express
+// const bp = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+
+app.use(cors());
+app.use(morgan('tiny'));
+// app.use(bp.json());
+app.use(express.json());
+
+// MongoDB
+
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const { connection } = mongoose;
+
+connection.once('open', () => {
+    console.log('MongoDB Atlas connection established successfully');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
